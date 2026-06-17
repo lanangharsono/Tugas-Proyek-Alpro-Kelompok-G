60aab7455903b4a16a35e341c4da7232960746d9
RIZKY AULIA PUTRI <rizky.aulia.p@mhs.unsoed.ac.id>
2026-06-18 06:36:46 +0700

    Add files via upload

---
+++ src_R/04_NIP_ASN.R
@@ -0,0 +1,44 @@
+#Program NIP_ASN 
+#Skenario 1 :199410212019031010
+#Skenario 2 :200002292022011002
+#Skenario 3 :200302292024011004
+#Inisialisasi kamus bulan dan batas hari
+data_bulan <- c("Januari", "Februari", "Maret", "April", "Mei", "Juni",
+                "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+)
+data_hari <- c(31, 28, 31, 30, 31, 30, 
+               31, 31, 30, 31, 30, 31
+)
+while (TRUE) {
+  #Menerima input dari user 
+  NIP <- readline(prompt = "Masukkan 18 digit NIP Anda: ")
+  tahun <- as.numeric(substr(NIP, 1, 4)) #Indeks R mulai dari 1
+  bulan <- as.numeric(substr(NIP, 5, 6))
+  tanggal <- as.numeric(substr(NIP, 7, 8))
+  
+  #Validasi rentang bulan
+  if (bulan < 1 | bulan > 12){
+    cat("NIP tidak valid, harap masukkan ulang.\n\n")
+    next #Kembali ke "repeat"
+  }
+  
+  #Mengambil data dari kamus
+  nama_bulan <- data_bulan[bulan]
+  max_hari <- data_hari[bulan]
+  
+  #Cek tahun kabisat (ubdate batas Februari)
+  if (bulan == 2 & (tahun %% 400 == 0 | (tahun %% 4 == 0 & tahun %% 100 != 0))){
+    max_hari = 29
+  }
+  
+  #Validasi batas tanggal
+  if (tanggal <1 | tanggal > max_hari){
+    cat("NIP tidak valid, harap masukkan ulang.\n\n")
+    next #Kembali ke "repeat"
+  }
+  
+  #Jika semua valid
+  cat("Tanggal lahir Anda:", tanggal, nama_bulan, tahun, "\n")
+  break
+}
+
