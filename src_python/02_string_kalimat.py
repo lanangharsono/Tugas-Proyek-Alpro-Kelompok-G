dd3d2b456aa2814babdc98b6702ee18746e2183e
RIZKY AULIA PUTRI <rizky.aulia.p@mhs.unsoed.ac.id>
2026-06-25 15:56:55 +0700

    Update 02_string_kalimat.py

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -10,7 +10,7 @@ Original file is located at
 #Program String_Kalimat
 K1 = "Saya tak 'kan menyerah."
 K2 = 'Ia berkata, "Aku menyayangimu."'
-K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Mechine Learning!\""
+K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
 K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
 print(K1)
 print(K2)
